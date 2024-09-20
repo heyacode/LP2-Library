f9cf3719e80cc17340458e1c422f5af1524a982a
yasmine-hayhay <compte.cm1@gmail.com>
2024-09-21 00:03:52 +0100

    Reader page - 1

--- frontend/src/components/Client/Client.jsx
+++ frontend/src/components/Client/Client.jsx
@@ -1,49 +1,66 @@
 // Clients.jsx
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import './clients.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./clients.css";
 
 const Clients = () => {
   const [clients, setClients] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/api/v1/client')
-      .then(response => setClients(response.data))
-      .catch(error => console.error(error));
+    axios
+      .get("http://localhost:3001/api/v1/client")
+      .then((response) => setClients(response.data))
+      .catch((error) => console.error(error));
   }, []);
 
   const handleDelete = (id) => {
-    axios.delete(`http://localhost:3001/api/v1/client/delete/${id}`)
+    axios
+      .delete(`http://localhost:3001/api/v1/client/delete/${id}`)
       .then(() => {
-        setClients(clients.filter(client => client._id !== id));
+        setClients(clients.filter((client) => client._id !== id));
       })
-      .catch(error => {
-        console.error('Error deleting client:', error);
+      .catch((error) => {
+        console.error("Error deleting client:", error);
       });
   };
   return (
     <div>
-
-      <div className='tab-cl'>
-        <table className='table'>
+      <div className="tab-cl">
+        <table className="table">
           <tr>
-            <td colspan="3"><h2 className='h2'>Readers List</h2></td>
-            <td><button className='ajt'><Link to="/clients/nouveau" className='link'>Add a new reader</Link></button></td></tr>
-          <tr >
+            <td colSpan={3}>
+              <h2 className="h2">Readers List</h2>
+            </td>
+            <td>
+              <button className="ajt">
+                <Link to="/clients/nouveau" className="link">
+                  Add a new reader
+                </Link>
+              </button>
+            </td>
+          </tr>
+          <tr>
             <th>Full Name</th>
             <th>Email</th>
             <th>Action</th>
           </tr>
-          {clients.map(client => (
-            <tr className='tr'>
-              <td>{client.nom} {client.prenom}</td>
+          {clients.map((client) => (
+            <tr className="tr">
+              <td>
+                {client.nom} {client.prenom}
+              </td>
               <td>{client.email}</td>
               <td>
                 <Link to={`/clients/${client._id}/editer`}>
-                  <button className='button-3'>Edit</button>
+                  <button className="button-3">Edit</button>
                 </Link>
-                <button onClick={() => handleDelete(client._id)} className='button-4'>Delete</button>
+                <button
+                  onClick={() => handleDelete(client._id)}
+                  className="button-4"
+                >
+                  Delete
+                </button>
               </td>
             </tr>
           ))}
